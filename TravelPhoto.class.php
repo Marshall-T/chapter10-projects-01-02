04a6c9fbfc4f6735224279f3cea8a1fcd01a6daf
Marshall-T <mjtshopper@gmail.com>
2016-12-17 20:19:23 -0600

    constructors

--- TravelPhoto.class.php
+++ TravelPhoto.class.php
@@ -4,6 +4,7 @@
  *Chapter 10 P1
  */
 
+//  class declaration
 class TravelPhoto {
     public static $photoID = 0;
     private $date;
@@ -13,19 +14,42 @@ class TravelPhoto {
     private $latitude;
     private $longitude;
     private $ID;
-    
+
+    // constructor   
     function __construct($fileName, $title, $description, $latitude, $longitude) {
-        $this->fileName = $fileName;
-        $this->title = $title;
-        $this->description = $description;
-        $this->latitude = $latitude;
-        $this->longitude = $longitude;
+        $this->setfileName = $fileName;
+        $this->settitle = $title;
+        $this->setdescription = $description;
+        $this->setlatitude = $latitude;
+        $this->setlongitude = $longitude;
         self::$ID++;
     }
+
+    public function getfileName() { return $this->fileName;}
+    public function gettitle() { return $this->title;}
+    public function getdescription() { return $this->description;}
+    public function getLatitude() { return $this->latitude;}
+    public function getLongitude() { return $this->longitude;}
     
+    public function setfileName() {$this->fileName = $fileName;}
+    public function settitle() {$this->title = $title;}
+    public function setdescription() {$this->description = $description;}
+    public function setlatitude() {$this->latitude = $latitude;}
+    public function setlongitude() {$this->longitude = $longitude;}
+    
+    public function __toString () {
+        $line = "File name: " . $this->getfileName();
+        $line .= "Title: " . $this->gettitle();
+        $line .= "Description: " . $this->getdescription();
+        $line .= "Latitude: " . $this->getlatitude();
+        $line .= "Longitude: " . $this->longitude();
+        return $line;
+    }
+
+/*  ??
     function __toString(){
         return 'test';
-/*
+
             <div class="col-md-3">
                 <a href="travel-image.php?id=<?php echo $img['id']; ?>" class="thumbnail">
                 <img src="images/travel/square/<?php echo $img['path']; ?>" alt="...">
@@ -36,7 +60,8 @@ class TravelPhoto {
         
         '<img src="$fileName" alt="title" title="$title" ></img>';
                 <img src="images/travel/square/6114850721.jpg" alt="...">
-*/    }
+    }
+*/
 }
 ?>
 
